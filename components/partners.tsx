053fb1b7a011044258580af904c76e501d65ed69
Friska Ammo <friskayanti99@gmail.com>
2026-04-26 10:23:17 +0800

    update: modify partners styling

--- components/partners.tsx
+++ components/partners.tsx
@@ -13,7 +13,7 @@ const logos = [...partners, ...partners]
 
 export default function Partners() {
   return (
-    <section className="py-16 bg-white overflow-hidden">
+    <section className="py-16 bg-pearl overflow-hidden">
       <div className="mx-auto max-w-7xl px-6 lg:px-8 mb-10 text-center">
         <p className="text-xs font-semibold uppercase tracking-[0.25em] text-muted-foreground">
           Trusted by Industry Leaders
@@ -22,15 +22,15 @@ export default function Partners() {
 
       {/* Marquee */}
       <div className="relative">
-        <div className="absolute top-0 bottom-0 left-0 w-32 z-10 bg-gradient-to-r from-white to-transparent pointer-events-none" />
-        <div className="absolute top-0 bottom-0 right-0 w-32 z-10 bg-gradient-to-l from-white to-transparent pointer-events-none" />
+        <div className="absolute top-0 bottom-0 left-0 w-32 z-10 bg-gradient-to-r from-pearl to-transparent pointer-events-none" />
+        <div className="absolute top-0 bottom-0 right-0 w-32 z-10 bg-gradient-to-l from-pearl to-transparent pointer-events-none" />
 
         <div className="flex overflow-hidden">
           <div className="flex animate-marquee items-center">
             {logos.map((partner, i) => (
               <div
                 key={`logo-${i}`}
-                className="flex-shrink-0 mx-12 relative h-12 w-32 grayscale opacity-40 hover:grayscale-0 hover:opacity-100 transition-all duration-500"
+                className="flex-shrink-0 mx-6 relative h-12 w-32 grayscale opacity-40 hover:grayscale-0 hover:opacity-100 transition-all duration-500"
               >
                 <Image
                   src={partner.src}
