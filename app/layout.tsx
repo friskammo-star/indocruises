2212dde3f6f94dbb72fb9f84bf0301c496de4383
friskammo <friskammo@github.com>
2026-04-29 19:35:45 +0800

    Update layout and add images

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,10 +19,31 @@ const jakarta = Plus_Jakarta_Sans({
 export const metadata: Metadata = {
   title: 'Indocruises Maritime Services | Professional Yacht Services in Indonesia',
   description: 'Indocruises Maritime Services - Full-service yacht management support for owners & operators in Indonesia. Expert maintenance, crew management, port clearance and more.',
-  generator: 'v0.app',
   icons: {
-    icon: '/favicon.ico',
+    icon: '/icon.png',
     apple: '/apple-icon.png',
+  },
+  openGraph: {
+    title: 'Indocruises Maritime Services',
+    description: 'Professional Yacht Services in Indonesia',
+    url: 'https://indocruises.com',
+    siteName: 'Indocruises',
+    images: [
+      {
+        url: '/opengraph-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Indocruises Maritime Services Logo',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Indocruises Maritime Services',
+    description: 'Professional Yacht Services in Indonesia',
+    images: ['/twitter-image.png'],
   },
 }
 
