6ef3084b74da4f7abec73d2a144d38a2192ab55b
friskammo <friskammo@github.com>
2026-05-06 17:00:15 +0800

    Remove social images, update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,22 +28,13 @@ export const metadata: Metadata = {
     description: 'Professional Yacht Services in Indonesia',
     url: 'https://indocruises.com',
     siteName: 'Indocruises',
-    images: [
-      {
-        url: '/opengraph-image.png',
-        width: 1200,
-        height: 630,
-        alt: 'Indocruises Maritime Services Logo',
-      },
-    ],
     locale: 'en_US',
     type: 'website',
   },
   twitter: {
-    card: 'summary_large_image',
+    card: 'summary',
     title: 'Indocruises Maritime Services',
     description: 'Professional Yacht Services in Indonesia',
-    images: ['/twitter-image.png'],
   },
 }
 
